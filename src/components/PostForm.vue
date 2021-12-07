f714347d8f7a5f702bfa3c2226a7cfef01974064
Ivan Torres Garcia <ivantorresgarcia.02@gmail.com>
2021-12-07 09:55:50 +0100

    feat(#4):Post getById Service

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -4,7 +4,7 @@
       Titulo: <input type="text" v-model="post.title"><br>
       Descripción: <input type="text" v-model="post.body"><br>
       Usuario: <input type="text" v-model="post.user"><br>
-      <button>Generar</button>
+      <button>Crear</button>
     </form>
   </div>
 </template>
